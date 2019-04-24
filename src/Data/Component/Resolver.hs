8efe0f6c67eb6fdb0f22019bcee82eccbfdea555
myuon <ioi.joi.koi.loi@gmail.com>
2019-04-25 01:03:22 +0900

    refactor: update

--- src/Data/Component/Resolver.hs
+++ src/Data/Component/Resolver.hs
@@ -5,7 +5,7 @@ module Data.Component.Resolver (
   foldResult,
 ) where
 
-import qualified Data.Aeson as Aeson
+import Data.Aeson
 import qualified Data.Text as T
 import MiniLight
 import qualified Data.Component.AnimationLayer as AnimationLayer
@@ -13,26 +13,27 @@ import qualified Data.Component.Button as Button
 import qualified Data.Component.Layer as Layer
 import qualified Data.Component.MessageEngine as MessageEngine
 import qualified Data.Component.MessageLayer as MessageLayer
+import qualified Data.Component.Selection as Selection
 
-foldResult :: (String -> b) -> (a -> b) -> Aeson.Result a -> b
+foldResult :: (String -> b) -> (a -> b) -> Result a -> b
 foldResult f g r = case r of
-  Aeson.Error   err -> f err
-  Aeson.Success a   -> g a
+  Error   err -> f err
+  Success a   -> g a
 
 -- | Pre-defined resolver supports all components in this library.
 resolver :: Resolver
 resolver name props = case name of
-  "animation-layer" -> newComponent
-    =<< AnimationLayer.new (foldResult error id $ Aeson.fromJSON props)
+  "animation-layer" ->
+    newComponent =<< AnimationLayer.new (foldResult error id $ fromJSON props)
   "button" ->
-    newComponent =<< Button.new (foldResult error id $ Aeson.fromJSON props)
-  "layer" ->
-    newComponent =<< Layer.new (foldResult error id $ Aeson.fromJSON props)
-  "message-engine" -> newComponent
-    =<< MessageEngine.new (foldResult error id $ Aeson.fromJSON props)
-  "message-layer" -> newComponent
-    =<< MessageLayer.new (foldResult error id $ Aeson.fromJSON props)
-  "tiled-layer" -> newComponent
-    =<< Layer.newNineTile (foldResult error id $ Aeson.fromJSON props)
+    newComponent =<< Button.new (foldResult error id $ fromJSON props)
+  "layer" -> newComponent =<< Layer.new (foldResult error id $ fromJSON props)
+  "message-engine" ->
+    newComponent =<< MessageEngine.new (foldResult error id $ fromJSON props)
+  "message-layer" ->
+    newComponent =<< MessageLayer.new (foldResult error id $ fromJSON props)
+  "tiled-layer" ->
+    newComponent =<< Layer.newNineTile (foldResult error id $ fromJSON props)
+  "selection" ->
+    newComponent =<< Selection.new (foldResult error id $ fromJSON props)
   _ -> error $ T.unpack $ "Component not defined: `" <> name <> "`"
-
---
+++ src/Data/Component/Selection.hs
@@ -0,0 +1,66 @@
+module Data.Component.Selection where
+
+import Control.Monad.State
+import Data.Aeson hiding ((.=))
+import qualified Data.Text as T
+import qualified Data.Vector as V
+import Lens.Micro
+import Lens.Micro.Mtl
+import Linear
+import MiniLight
+import qualified SDL.Font
+import qualified SDL.Vect as Vect
+import qualified Data.Component.Basic as Basic
+import qualified Data.Component.Layer as Layer
+
+data Selection = Selection {
+  layer :: Layer.Layer,
+  font :: SDL.Font.Font,
+  hover :: Maybe Int,
+  conf :: Config
+}
+
+hoverL :: Lens' Selection (Maybe Int)
+hoverL = lens hover (\env r -> env { hover = r })
+
+instance ComponentUnit Selection where
+  update = return
+
+  figures comp = do
+    let p = Vect.V2 15 10
+    textTextures <- V.forM (V.indexed $ labels $ conf comp) $ \(i,label) -> liftMiniLight $ fmap (translate (p + Vect.V2 0 (i * 30))) $ text (font comp) (Basic.fontColor $ basic $ conf comp) label
+    base <- figures (layer comp)
+    highlight <- liftMiniLight $ rectangleFilled (Vect.V4 240 240 240 40) $ _y .~ 30 $ Basic.size (basic (conf comp))
+
+    return $ base
+      ++ map (translate (Basic.position $ basic $ conf comp)) (translate (Vect.V2 0 (maybe 0 id (hover comp) * 30 + p ^. _y)) highlight
+      : V.toList textTextures)
+
+  useCache c1 c2 = hover c1 == hover c2
+
+  onSignal = Basic.wrapSignal (basic . conf) $ \ev sel -> flip execStateT sel $ do
+    uid <- view uidL
+
+    case ev `asSignal` uid of
+      Just (Basic.MouseOver pos) | (pos ^. _y) `div` 30 <= V.length (labels (conf sel)) - 1 -> do
+        hoverL .= Just ((pos ^. _y) `div` 30)
+      _ -> return ()
+
+data Config = Config {
+  basic :: Basic.Config,
+  labels :: V.Vector T.Text,
+  image :: FilePath
+}
+
+instance FromJSON Config where
+  parseJSON = Basic.wrapConfig (\b (l,i) -> return $ Config b l i) $ \v -> do
+    labels <- v .: "labels" .!= V.empty
+    image <- v .: "image"
+
+    return (labels, image)
+
+new :: Config -> MiniLight Selection
+new conf = do
+  font  <- Basic.loadFontFrom (basic conf)
+  layer <- Layer.newNineTile $ Layer.Config (basic conf) (image conf)
+  return $ Selection {font = font, conf = conf, hover = Nothing, layer = layer}
