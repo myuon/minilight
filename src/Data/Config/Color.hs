6e690aa5399ecced01adf118ca6aeadb50ea6593
myuon <ioi.joi.koi.loi@gmail.com>
2019-04-26 23:56:28 +0900

    feat: Color config

--- src/Data/Config/Color.hs
+++ src/Data/Config/Color.hs
@@ -2,7 +2,6 @@ module Data.Config.Color where
 
 import Data.Aeson
 import Data.Word (Word8)
-import MiniLight
 import qualified SDL.Vect as Vect
 
 data Config = Config {
@@ -13,10 +12,9 @@ data Config = Config {
 }
 
 asV4 :: Config -> Vect.V4 Word8
-asV4 (Config r g b a) = Vect.V4 r g b a
+asV4 (Config r g b a) = fmap toEnum $ Vect.V4 r g b a
 
 instance FromJSON Config where
   parseJSON = withObject "color" $ \v -> do
-    vec <- fromJSON v
-    return $ Config (vec VU.! 0) (vec VU.! 1) (vec VU.! 2) (vec VU.! 3)
-
+    [r,g,b,a] <- fmap (map toEnum) $ v .:? "color" .!= [255, 255, 255, 255]
+    return $ Config r g b a
