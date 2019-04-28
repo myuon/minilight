24ebbe09ea8e76a94e74ed92552dd588c2ae4111
myuon <ioi.joi.koi.loi@gmail.com>
2019-04-28 21:27:58 +0900

    feat: improve diff algorithm

--- src/MiniLight/Component/Internal/Diff.hs
+++ src/MiniLight/Component/Internal/Diff.hs
@@ -2,13 +2,34 @@ module MiniLight.Component.Internal.Diff where
 
 import Data.Aeson
 import qualified Data.Aeson.Diff as Diff
+import Data.Aeson.Pointer
+import Data.Aeson.Patch
+import Data.Maybe (catMaybes)
 import MiniLight.Component.Internal.Types
+import Debug.Trace
 
 data DiffType
   = New
   | Modify
   | Delete
+  deriving (Eq, Show)
 
-diff :: AppConfig -> AppConfig -> [Diff.Operation]
-diff conf1 conf2 =
-  Diff.patchOperations $ Diff.diff (toJSON conf1) (toJSON conf2)
+diff :: AppConfig -> AppConfig -> [(DiffType, Value)]
+diff conf1@(AppConfig comps) conf2 =
+  catMaybes $ map go $ Diff.patchOperations $ Diff.diff (toJSON conf1)
+                                                        (toJSON conf2)
+ where
+  go (Add (Pointer [OKey "app", AKey _]) v) = Just (New, v)
+  go (Rep (Pointer (OKey "app":AKey n:path)) v) | head path /= OKey "uid" = Just
+    ( Modify
+    , (\(Success a) -> a)
+      $ Diff.applyOperation (Rep (Pointer path) v) (toJSON $ comps !! n)
+    )
+  go (Rep (Pointer (OKey "app":AKey n:path)) v) | otherwise = Nothing
+  go (Rem (Pointer [OKey "app", AKey n])) = Just (Delete, toJSON $ comps !! n)
+  go (Rem (Pointer (OKey "app":AKey n:path))) = Just
+    ( Modify
+    , (\(Success a) -> a)
+      $ Diff.applyOperation (Rem (Pointer path)) (toJSON $ comps !! n)
+    )
+  go op = trace ("unsupported edit: " ++ show op) `seq` Nothing
