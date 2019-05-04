47ce19dc247c26b25fe789f186f235099d974005
myuon <ioi.joi.koi.loi@gmail.com>
2019-05-04 17:58:51 +0900

    refactor: callCC with jailbreak

--- src/MiniLight/Component/Loader.hs
+++ src/MiniLight/Component/Loader.hs
@@ -13,14 +13,13 @@ module MiniLight.Component.Loader (
 import Control.Lens
 import Control.Monad
 import Control.Monad.Catch
+import Control.Monad.Cont
 import qualified Control.Monad.Caster as Caster
-import Control.Monad.IO.Class
 import Data.Aeson
 import qualified Data.Aeson.Diff as Diff
 import Data.Aeson.Patch
 import Data.Aeson.Pointer
 import Data.IORef
-import Data.Maybe (catMaybes)
 import qualified Data.Registry as R
 import qualified Data.Vector as V
 import Data.Yaml (decodeFileEither)
@@ -59,30 +58,35 @@ loadAppConfig
   => FilePath  -- ^ Filepath to the yaml file.
   -> Resolver  -- ^ Specify any resolver.
   -> LightT env m ()
-loadAppConfig path mapper = do
-  conf <- resolveConfig path
+loadAppConfig path mapper = flip runContT return $ callCC $ \jailbreak -> do
+  conf <- resolveConfig path >>= \case
+    Left e -> do
+      lift $ Caster.err e
+      jailbreak ()
+    Right r -> return r
 
-  (\m -> either Caster.err m conf) $ \conf -> do
-    confs <- fmap (V.mapMaybe id) $ V.forM (app conf) $ \conf -> do
-      uid    <- newUID
-      result <- liftMiniLight $ mapper (name conf) uid (properties conf)
+  confs <- lift $ fmap (V.mapMaybe id) $ V.forM (app conf) $ \conf -> do
+    uid    <- newUID
+    result <- liftMiniLight $ mapper (name conf) uid (properties conf)
 
-      flip (either (\err -> Caster.err err >> return Nothing)) result
-        $ (fmap Just .)
-        $ \component -> do
-            register component
+    case result of
+      Left e -> do
+        Caster.err e
+        return Nothing
+      Right component -> do
+        register component
 
-            Caster.info
-              $  "Component loaded: {name: "
-              <> show (name conf)
-              <> ", uid = "
-              <> show uid
-              <> "}"
+        Caster.info
+          $  "Component loaded: {name: "
+          <> show (name conf)
+          <> ", uid = "
+          <> show uid
+          <> "}"
 
-            return (uid, conf)
+        return $ Just (uid, conf)
 
-    ref <- view _appConfig
-    liftIO $ writeIORef ref $ AppConfig (V.map snd confs) (V.map fst confs)
+  ref <- view _appConfig
+  liftIO $ writeIORef ref $ AppConfig (V.map snd confs) (V.map fst confs)
 
 -- | Load the config file again and place the newly loaded components. This is used for HCR (hot component replacement).
 -- Call 'loadAppConfig' first.
@@ -91,50 +95,55 @@ patchAppConfig
   => FilePath  -- ^ Filepath to the yaml file.
   -> Resolver  -- ^ Specify any resolver.
   -> LightT env m ()
-patchAppConfig path resolver = do
+patchAppConfig path resolver = flip runContT return $ callCC $ \jailbreak -> do
   cref      <- view _appConfig
   appConfig <- liftIO $ readIORef cref
 
   compsV    <- liftIO $ V.unsafeThaw (app appConfig)
   uuidsV    <- liftIO $ V.unsafeThaw (uuid appConfig)
 
-  mconf'    <- resolveConfig path
+  conf'     <- do
+    mconf <- lift $ resolveConfig path
 
-  case mconf' of
-    Left e -> Caster.err e
-    Right conf' ->
-      forM_
-          ( Diff.patchOperations
-          $ Diff.diff (toJSON $ app appConfig) (toJSON $ app conf')
-          )
-        $ \op -> do
-            Caster.info $ "CMR detected: " <> show op
+    case mconf of
+      Left e -> do
+        lift $ Caster.err e
+        jailbreak ()
+      Right r -> return r
 
-            case op of
-              Add path@(Pointer [OKey "app", AKey i]) v -> do
-                let conf = (\(Success a) -> a) $ fromJSON v
+  lift
+    $ forM_
+        ( Diff.patchOperations
+        $ Diff.diff (toJSON $ app appConfig) (toJSON $ app conf')
+        )
+    $ \op -> do
+        Caster.info $ "CMR detected: " <> show op
 
-                newID  <- newUID
-                result <- liftMiniLight
-                  $ resolver (name conf) newID (properties conf)
+        case op of
+          Add path@(Pointer [OKey "app", AKey i]) v -> do
+            let conf = (\(Success a) -> a) $ fromJSON v
 
-                either
-                  ( \e -> do
-                    Caster.err $ "Component creation failed: " <> e
-                    return Nothing
-                  )
-                  ( \c -> do
-                    register c
-                    Caster.info
-                      $  "Component inserted: {name: "
-                      <> show (name conf)
-                      <> ", uid = "
-                      <> show (uid conf)
-                      <> "}"
-                    return $ Just op
-                  )
-                  result
-              _ -> return Nothing
+            newID  <- newUID
+            result <- liftMiniLight
+              $ resolver (name conf) newID (properties conf)
+
+            either
+              ( \e -> do
+                Caster.err $ "Component creation failed: " <> e
+                return Nothing
+              )
+              ( \c -> do
+                register c
+                Caster.info
+                  $  "Component inserted: {name: "
+                  <> show (name conf)
+                  <> ", uid = "
+                  <> show (uid conf)
+                  <> "}"
+                return $ Just op
+              )
+              result
+          _ -> return Nothing
 
 -- | Register a component to the component registry.
 register :: (HasLoaderEnv env, MonadIO m) => Component -> LightT env m ()
