7b7918c73ec58b99662e585e739e7b795da7f311
myuon <ioi.joi.koi.loi@gmail.com>
2019-05-05 16:37:37 +0900

    feat(MessageEngine): introduce page

--- src/Data/Component/MessageEngine.hs
+++ src/Data/Component/MessageEngine.hs
@@ -6,14 +6,16 @@ import Control.Monad.State
 import Data.Aeson
 import qualified Data.Config.Font as Font
 import qualified Data.Text as T
+import qualified Data.Vector as V
 import Data.Typeable
 import MiniLight
 import qualified SDL
 import qualified SDL.Font
 import qualified SDL.Vect as Vect
 
+-- | 'MessageEngine' configuration. If @static@ enabled, only the first page will be rendered.
 data Config = Config {
-  messages :: T.Text,
+  messages :: V.Vector T.Text,  -- ^ paged messages
   static :: Bool,
   font :: Font.Config
 }
@@ -31,7 +33,8 @@ instance FromJSON Config where
 data MessageEngine = MessageEngine {
   fontData :: SDL.Font.Font,
   counter :: Int,
-  rendered :: Int,
+  page :: Int,
+  textCounter :: Int,
   textTexture :: Figure,
   finished :: Bool,
   config :: Config
@@ -50,22 +53,22 @@ instance ComponentUnit MessageEngine where
 
     unless (finished comp) $ do
       when (counter comp `mod` 10 == 0) $ do
-        id %= (\c -> c { rendered = rendered c + 1 })
+        id %= (\c -> c { textCounter = textCounter c + 1 })
 
         comp <- get
-        when (rendered comp == T.length (messages (config comp))) $ do
+        when (comp ^. _textCounter == T.length ((config comp ^. _messages) V.! (comp ^. _page))) $ do
           id %= (\c -> c { finished = True })
 
       id %= (\c -> c { counter = counter c + 1 })
 
   figures comp = do
-    (w, h) <- SDL.Font.size (comp ^. _fontData) (T.take (rendered comp) $ messages (config comp))
+    (w, h) <- SDL.Font.size (comp ^. _fontData) (T.take (comp ^. _textCounter) $ (config comp ^. _messages) V.! (comp ^. _page))
 
     return [
       clip (SDL.Rectangle 0 (Vect.V2 w h)) $ textTexture comp
       ]
 
-  useCache c1 c2 = rendered c1 == rendered c2
+  useCache c1 c2 = page c1 == page c2 && textCounter c1 == textCounter c2
 
   onSignal ev c = view uidL >>= \u -> go c (ev,u)
     where
@@ -75,12 +78,13 @@ instance ComponentUnit MessageEngine where
 new :: Config -> MiniLight MessageEngine
 new conf = do
   font        <- Font.loadFontFrom (font conf)
-  textTexture <- text font (conf ^. _font ^. Font._color) $ messages conf
+  textTexture <- text font (conf ^. _font ^. Font._color) $ messages conf V.! 0
 
   return $ MessageEngine
     { fontData    = font
     , counter     = 0
-    , rendered    = if static conf then T.length (messages conf) else 0
+    , page        = 0
+    , textCounter = if static conf then T.length (messages conf V.! 0) else 0
     , textTexture = textTexture
     , finished    = static conf
     , config      = conf
