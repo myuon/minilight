ac631235f63f418ab95ebb67c15d96d121eb82d0
myuon <ioi.joi.koi.loi@gmail.com>
2019-05-05 11:49:59 +0900

    chore: update publish script

--- publish.sh
+++ publish.sh
@@ -10,6 +10,6 @@ cabal new-sdist
 dir=$(mktemp -d dist-docs.XXXXXX)
 trap 'rm -r "$dir"' EXIT
 
-cabal new-haddock --builddir="$dir" --haddock-for-hackage --haddock-option=--hyperlinked-source && \
+cabal new-haddock --builddir="$dir" --haddock-for-hackage --enable-documentation --haddock-option=--hyperlinked-source && \
 cabal upload --publish dist-newstyle/sdist/minilight-$1.tar.gz && \
 cabal upload --publish -d $dir/minilight-$1-docs.tar.gz
